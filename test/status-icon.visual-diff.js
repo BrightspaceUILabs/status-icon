75933755b5bc37919553d3ec9f631b05bd6d4b6f
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-12-06 09:23:33 -0500

    feat: switching to ESM-by-default & upgrading actions (#14)

--- test/status-icon.visual-diff.js
+++ test/status-icon.visual-diff.js
@@ -1,9 +1,9 @@
-const puppeteer = require('puppeteer');
-const VisualDiff = require('@brightspace-ui/visual-diff');
+import puppeteer from 'puppeteer';
+import { VisualDiff } from '@brightspace-ui/visual-diff';
 
 describe('d2l-labs-status-icon', () => {
 
-	const visualDiff = new VisualDiff('status-icon', __dirname);
+	const visualDiff = new VisualDiff('status-icon', import.meta.url);
 
 	let browser, page;
 
