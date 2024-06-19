df85196ffbdbccac82783c0cc3b66aac7d0800b2
Craig Reynolds <ducttape111@hotmail.com>
2024-06-19 14:21:00 -0400

    oops, I did it backwards

--- test/status-icon.test.js
+++ test/status-icon.test.js
@@ -1,5 +1,5 @@
-import { expect, fixture, html, runConstructor } from '@brightspace-ui/testing';
 import '../status-icon.js';
+import { expect, fixture, html, runConstructor } from '@brightspace-ui/testing';
 
 describe('d2l-labs-status-icon', () => {
 
--- test/status-icon.vdiff.js
+++ test/status-icon.vdiff.js
@@ -1,5 +1,5 @@
-import { expect, fixture, html } from '@brightspace-ui/testing';
 import '../status-icon.js';
+import { expect, fixture, html } from '@brightspace-ui/testing';
 
 describe('d2l-labs-status-icon', () => {
 
