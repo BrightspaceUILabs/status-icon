424fe2f50f640a34740727116e8c3036ab072159
Dave Lockhart <dlockhart@users.noreply.github.com>
2024-06-19 14:26:19 -0400

    add message

--- test/status-icon.vdiff.js
+++ test/status-icon.vdiff.js
@@ -3,9 +3,9 @@ import { expect, fixture, html } from '@brightspace-ui/testing';
 
 describe('d2l-labs-status-icon', () => {
 
-	['failure', 'warning', 'success'].forEach((state) => {
+	['Failure', 'Warning', 'Success'].forEach((state) => {
 		it(state, async() => {
-			const elem = await fixture(html`<d2l-labs-status-icon state=${state}></d2l-labs-status-icon>`);
+			const elem = await fixture(html`<d2l-labs-status-icon state="${state.toLowerCase()}" message="${state}"></d2l-labs-status-icon>`);
 			await expect(elem).to.be.golden();
 		});
 	});
